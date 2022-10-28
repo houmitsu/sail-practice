5902eb909e41ca0d349ebc9a7ec2c1e31e0975ae
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-10-28 23:07:54 +0900

    title

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -7,7 +7,10 @@
     <a href="{{ route('test.create') }}">create</a>
 </div>
 @foreach ($items as $item)
-    <div><img src="{{ Storage::url($item->img_path) }}" width="25%"></div>
+    <div>
+        <div>{{$item->title}}</div>
+        <img src="{{ Storage::url($item->img_path) }}" width="25%">
+    </div>
 @endforeach
 
 @endsection
